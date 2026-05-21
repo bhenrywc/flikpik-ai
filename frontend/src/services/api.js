7628efb384daa103aa9001e936fd9ecfc1ea871a
bhenrywc <68617445+bhenrywc@users.noreply.github.com>
2026-05-21 13:07:17 +0000

    updatedijs

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000";
+const API_BASE_URL = "https://scaling-succotash-j99j96p97vvcj7gw-8000.app.github.dev";
 
 export async function getPopularMovies() {
   const response = await axios.get(`${API_BASE_URL}/recommendations/popular`);
